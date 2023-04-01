965dafd35a8a8d212c573358843490471ece20c5
kakshay21 <k.akshay9721@gmail.com>
2023-04-01 23:31:30 +0530

    Update nvim setup

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "java", "kotlin", "c", "lua", "rust" },
+  ensure_installed = { "help", "java", "kotlin", "c", "lua", "rust", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -19,5 +19,8 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+      enable = true
+  }
 }
 
