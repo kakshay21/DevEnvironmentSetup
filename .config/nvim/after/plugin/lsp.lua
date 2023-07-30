6c55041683cc8e1f20651b995104473d6e153b6c
kakshay21 <k.akshay9721@gmail.com>
2023-07-30 18:50:01 +0530

    Remove depricated configurations

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -3,21 +3,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'sumneko_lua',
     'rust_analyzer',
-    'kotlin_language_server',
-    'jdtls',
-})
-
--- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
 })
 
 
